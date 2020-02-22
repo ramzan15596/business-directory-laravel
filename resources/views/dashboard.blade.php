e104ae69c7e80f2cb135a758bc752caf92ad6883
Muhammad Ramzan <ramzan11596@gmail.com>
2020-02-22 06:50:05 +0500

    upload project

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                        <h1>Your Listings</h1>
+                    @if(count($listings)>0)
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Company</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach($listings as $listing)
+                                <tr>
+                                    <td>{{ $listing->name }}</td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
